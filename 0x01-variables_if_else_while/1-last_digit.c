1e1726c840ead0ef5a3002ecc754f7fba3f88ee7
elisa2105 <aikomi1726@gmail.com>
2019-09-19 19:47:35 +0000

    conditional and loops in c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -14,10 +14,10 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 last = n % 10;
 if (last > 5)
-printf("Last digit of %d is and is greater than 5\n", n);
+printf("Last digit of %d is %d and is greater than 5\n", n, last);
 if (last == 0)
-printf("Last digit of %d is and is 0\n", n);
+printf("Last digit of %d is %d and is 0\n", n, last);
 if (last < 6 && last != 0)
-printf("Last digit of %d is less than 6 and not 0\n", n);
+printf("Last digit of %d is %d less than 6 and not 0\n", n, last);
 return (0);
 }
