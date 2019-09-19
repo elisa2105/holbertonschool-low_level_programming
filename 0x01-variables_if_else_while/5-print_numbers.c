47837a997f1b347171c8cab795f9d451ea041895
elisa2105 <aikomi1726@gmail.com>
2019-09-19 21:14:42 +0000

    conditional and loops in c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int firstChar = 0;
+for (; firstChar < 10 ; firstChar++)
+printf("%d", firstChar);
+printf("\n");
+return (0);
+}
