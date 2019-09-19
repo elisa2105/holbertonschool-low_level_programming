874d5ecc8186182ccfbad89c63b32c7929663201
elisa2105 <aikomi1726@gmail.com>
2019-09-19 20:24:23 +0000

    conditional and loops in c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int firstChar = 0;
+int firstCharUpper = 0;
+for (firstChar = 97 ; firstChar < 123 ; firstChar++)
+putchar (firstChar);
+for (firstCharUpper = 65 ; firstCharUpper < 90 ; firstCharUpper++)
+putchar (firstCharUpper);
+putchar ('\n');
+return (0);
+}
