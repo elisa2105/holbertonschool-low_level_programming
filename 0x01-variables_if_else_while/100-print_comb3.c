b080ee04819ac36982de028ccb514315b38ac025
elisa2105 <aikomi1726@gmail.com>
2019-09-19 21:00:28 -0500

    conditional and loops

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int firstChar = 0;
+int secondChar = 0;
+int flap = 0;
+for (firstChar = 48 ; firstChar < 58 ; firstChar++)
+{
+for (secondChar = 48 ; secondChar < 58 ; secondChar++)
+{
+if (firstChar != secondChar && firstChar < secondChar)
+{
+putchar(firstChar);
+putChar(secondChar);
+flap = 0;
+}
+else
+flap = 1;
+if ((firstChar < 57 || secondChar < 57) && flap == 0)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+putchar('\n');
+return (0);
+}
