2db579e948a65338225789a818ef028f4584fcb8
elisa2105 <aikomi1726@gmail.com>
2019-09-24 19:52:00 -0500

    functions

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+* add - Entry point
+* @a: integer to perate
+* @b: integer to operate
+* Description: Adds two numbers
+* Return: sum a + b
+*/
+int add(int a, int b)
+{
+int sum;
+sum = a + b;
+return (sum);
+}
