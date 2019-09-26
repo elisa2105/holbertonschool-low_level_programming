d523d968b5f28c30e79d57946c833a8f1e607e0a
elisa2105 <aikomi1726@gmail.com>
2019-09-26 17:48:21 +0000

    print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ * print_numbers - entry point
+ * Description: prints the numbers from 0 to 9
+ * Return: void
+ */
+void print_numbers(void)
+{
+int i;
+for (i = 48; i < 58; i++)
+{
+_putchar(i);
+}
+_putchar('\n');
+}
