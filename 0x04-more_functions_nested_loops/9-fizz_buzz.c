bf58ef447e4e887009d1b71832dc32fa2df69f86
elisa2105 <aikomi1726@gmail.com>
2019-09-26 21:08:04 +0000

    square v3 fixed

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Description: prints 10 times the numbers from 0 to 14
+ * Return: void
+ */
+int main(void)
+{
+int i;
+
+for (i = 0; i < 100; i++)
+{
+if (i % 15 == 0)
+printf("FizzBuzz");
+else
+{
+if (i % 3 == 0)
+printf("Fizz");
+else
+{
+if (i % 5 == 0)
+printf("Buzz");
+else
+printf("%d", i);
+}
+}
+if (i != 100)
+printf(" ");
+}
+printf("\n");
+return (0);
+}
