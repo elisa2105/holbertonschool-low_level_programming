7cfeaeb6237ecd1146a64aeb9040b799c94b3ab7
elisa2105 <aikomi1726@gmail.com>
2019-09-26 19:37:16 +0000

    diagonal fixed

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+/**
+ * print_diagonal - entry point
+ * @n: Integer
+ * Description: draws a diagonal line on the terminal.
+ * Return: void
+ */
+void print_diagonal(int n)
+{
+int i;
+int j;
+if (n <= 0)
+_putchar('\n');
+else
+for (i = 1; i <= n; i++)
+{
+for (j = 1 ; j <= i ; j++)
+{
+if (j == i)
+_putchar('\\');
+else
+_putchar(' ');
+}
+_putchar('\n');
+}
+}
