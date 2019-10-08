64a653f4798a71809317b05b735ad015a5d3b54d
elisa2105 <aikomi1726@gmail.com>
2019-10-08 01:08:34 +0000

    sets the value of a pointer to a char.

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,14 @@
+include "holberton.h"
+/**
+ * *_strpbrk - searches a string for any of a set of bytes.
+ * @s: the address of memory area pointed.
+ * @accept: string
+ * Return: a pointer to the memory area.
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+
+
+return (s);
+}
---
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+ * set_string - sets the value of a pointer to a char.
+ * @s: the address of memory area pointed
+ * @to: char
+ */
+
+void set_string(char **s, char *to)
+{
+*s=to;
+}
