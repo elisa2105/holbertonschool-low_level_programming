26b2ce16e169cea2c1e83f53b93137d99b509f1f
elisa2105 <aikomi1726@gmail.com>
2019-10-12 21:17:54 +0000

    adds positive numbers

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+/**
+ * main - result of add two numbers
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: 1 and error if the numbers is not digit
+ */
+int main(int argc, char *argv[])
+{
+int cont;
+int a;
+if (argc < 2)
+{
+printf("0\n");
+}
+else
+{
+for (cont = 1; cont < argc; cont++)
+{
+if (isdigit(*argv[cont]))
+a += atoi(argv[cont]);
+else
+{
+printf("Error\n");
+return (1);
+}
+}
+printf("%d\n", a);
+}
+return (0);
+}
