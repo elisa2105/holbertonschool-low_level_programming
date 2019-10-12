4f71537202b5bb37c78a529aa02d6f0b03e010a0
elisa2105 <aikomi1726@gmail.com>
2019-10-12 19:07:07 +0000

    multiplies two numbers

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - result of the multiplication for two numbers
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: 1 and error if it dooes not receive any argument
+*/
+int main(int argc, char *argv[])
+{
+if (argc < 2)
+{
+printf("Error\n");
+return (1);
+}
+else
+{
+int a = atoi(argv[1]);
+int b = atoi(argv[2]);
+printf("%d\n", a * b);
+return (0);
+}
+}
