f61a6d113748bab9f0c65c9d0b6ebfb8f3594af8
elisa2105 <aikomi1726@gmail.com>
2019-10-12 19:37:19 +0000

    print the result of the multiplication v3 fixed

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -9,12 +9,12 @@
 int main(int argc, char *argv[])
 {
 int a, b;
-if (argc < 2)
+if (argc <= 2)
 {
 printf("Error\n");
 return (1);
 }
-if (argc == 2)
+else
 {
 a = atoi(argv[1]);
 b = atoi(argv[2]);
