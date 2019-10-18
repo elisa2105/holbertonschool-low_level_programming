1b3a9908897daaee69695cb431b761efb84d0006
elisa2105 <aikomi1726@gmail.com>
2019-10-18 00:37:53 +0000

    allocates memory for an array using malloc fixed betty

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,15 +1,16 @@
 #include "holberton.h"
 #include <stdlib.h>
 /**
- * malloc_checked - allocates memory using malloc.
- * @b: unsigned int
+ * _calloc - allocates memory for an array.
+ * @size: unsigned int
+ * @nmemb: int
  * Return:  void
 */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 unsigned int i = 0;
 char *arr;
-if (size == 0 || nmemb == 0 )
+if (size == 0 || nmemb == 0)
 return (NULL);
 arr = malloc(nmemb * size);
 if (arr == NULL)
