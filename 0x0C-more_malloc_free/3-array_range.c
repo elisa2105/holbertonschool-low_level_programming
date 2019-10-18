02641341c6337a3bdc3e6c56473dd3dccaa443ad
elisa2105 <aikomi1726@gmail.com>
2019-10-18 00:53:26 +0000

    allocates memory for an array using malloc fixed betty

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+ * array_range - creates an array of integers.
+ * @min: int
+ * @max: int
+ * Return:  void
+*/
+int *array_range(int min, int max);
+{
+int i = 0;
+int *arr;
+if (min > max)
+return (NULL);
+arr = malloc(sizeof(int) * (max - min + 1));
+if (arr == NULL)
+return (NULL);
+for (; min <= max; i++, min++)
+arr[i] = min;
+return (arr);
+}
