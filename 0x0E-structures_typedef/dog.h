c6aa706a43e132c1a7793efc9550a627e61c4186
elisa2105 <aikomi1726@gmail.com>
2019-10-21 18:35:47 +0000

    Define a new type struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef HOLB
+#define HOLB
+/**
+ * struct dog - prints buffer in hexa
+ * @name: dog's name
+ * @age: age of the dog
+ * @owner: dog's owner
+ * Description define a dog
+ */
+typedef struct dog
+{
+char *name;
+float age;
+char *owner;
+} dog;
+#endif
