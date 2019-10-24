3b9b1659fdb0550a08af5f40f554fbdc35a5e778
elisa2105 <aikomi1726@gmail.com>
2019-10-24 01:55:37 +0000

    calc

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,32 @@
+#include "3-calc.h"
+#include <stdlib.h>
+/**
+ * get_op_func - searches for an integer.
+ * @s: Symbol
+ * Description: executes a function given as a parameter on an array.
+ * Return: integer
+ */
+int (*get_op_func(char *s))(int, int)
+{
+op_t ops[] = {
+{"+", op_add},
+{"-", op_sub},
+{"*", op_mul},
+{"/", op_div},
+{"%", op_mod},
+{NULL, NULL}
+};
+int i = 0;
+while (i < 5)
+{
+while (ops[i].op != NULL)
+{
+if (strcmp(s, ops[i].op) == 0)
+{
+return (ops[i].f);
+}
+i++;
+}
+}
+return (NULL);
+}
