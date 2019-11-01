ee93f934adf9be12af92601f9e8ee963e566af05
elisa2105 <aikomi1726@gmail.com>
2019-11-01 00:21:45 +0000

    sdm fixed registries

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,13 +1,14 @@
-; extern printf
-section data
+extern printf
+section .data
 msg: db "Hello, Holberton",0
 fmt: db "%s",10,0
 seccion.text
+
 global main
 main:
 push rbp
-mov rsi, fmt
-mov csi, msg
+mov rdi, fmt
+mov rsi, msg
 mov rax, 0
 call printf
 
