7bb5285892cdff588e2362376e8ed73f590b18b8
elisa2105 <aikomi1726@gmail.com>
2019-11-05 19:15:07 +0000

    frees a listint_t list. V2

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * reverse_listint - reverses a linked list.
+ * @head: type struct
+ * Return: node
+ */
+listint_t *reverse_listint(listint_t **head)
+{
+listint_t *an = NULL;
+listint_t *ac = *head;
+listint_t n;
+while (ac != NULL)
+{
+n = ac->next;
+ac->next = an;
+an = ac;
+ac = n;
+}
+*head = an;
+return (*head);
+}
