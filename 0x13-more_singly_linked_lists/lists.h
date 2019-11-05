bb12ff568a687ce83bcfe5106da0329899c2d274
elisa2105 <aikomi1726@gmail.com>
2019-11-05 15:11:56 +0000

    fixed betty lists.h

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,5 +1,6 @@
 #ifndef _HOLBERTON_H_
 #define _HOLBERTON_H_
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
@@ -13,8 +14,8 @@
  */
 typedef struct listint_s
 {
-    int n;
-    struct listint_s *next;
+int n;
+struct listint_s *next;
 } listint_t;
 size_t print_listint(const listint_t *h);
 
