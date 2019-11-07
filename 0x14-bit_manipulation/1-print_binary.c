d56444452211586b2c5893626905c651cfabadd5
elisa2105 <aikomi1726@gmail.com>
2019-11-07 16:32:41 +0000

    betty fixed

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -9,7 +9,7 @@ void print_binary(unsigned long int n)
 unsigned long int i = 1;
 while (i < n)
 {
-if ( i > i << 1)
+if (i > i << 1)
 break;
 i <<= 1;
 }
@@ -17,7 +17,7 @@ if (n != 0 && i > n)
 i >>= 1;
 while (i > 0)
 {
-if ((n & i ) == 0)
+if ((n & i) == 0)
 _putchar(48);
 else
 _putchar(49);
