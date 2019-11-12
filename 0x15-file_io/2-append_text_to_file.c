d77f6e73dbf3029825cbbbbc5b3cd6fdebd389f2
elisa2105 <aikomi1726@gmail.com>
2019-11-12 22:12:32 +0000

    fixed append

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -10,18 +10,18 @@ int append_text_to_file(const char *filename, char *text_content)
 int n;
 int i;
 int size = 0;
-if(filename == NULL)
+if (filename == NULL)
 return (-1);
-if(text_content == NULL)
+if (text_content == NULL)
 text_content = "";
 n = open(filename, O_WRONLY | O_APPEND);
-if(n == -1)
+if (n == -1)
 return (-1);
-while(text_content[size] != '\0')
+while (text_content[size] != '\0')
 size++;
 i = write(n, text_content, size);
-if(i == -1)
+if (i == -1)
 return (-1);
-close (n);
+close(n);
 return (1);
 }
