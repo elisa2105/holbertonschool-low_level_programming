1d53a10b32b836ffb031a564164a49a46b1e37d0
elisa2105 <aikomi1726@gmail.com>
2019-11-13 00:39:03 +0000

    copy

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,81 @@
+#include "holberton.h"
+/**
+ * main - Entry point
+ * @ac: number of arguments
+ * @av: arguments
+ * Return: Always 0.
+*/
+int main(int ac, char **av)
+{
+char *go;
+char *to;
+if (ac != 3)
+{
+dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+exit(97);
+}
+go = av[1];
+to = av[2];
+if (go == NULL)
+{
+_error1(go);
+}
+if (to == NULL)
+{
+_error1(to);
+}
+cp(go, to);
+return (0);
+}
+/**
+ * _error1- prints the error 98
+ * @xo: file with failure
+ */
+void _error1(char *xo)
+{
+dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", xo);
+exit(98);
+}
+
+/**
+ * cp - copy a file to other file
+ * @go: file to be copied
+ * @to: file
+ * Return: 1 on success, -1 on failure
+ */
+int cp(char *go, char *to)
+{
+int in, out, i, j;
+char buffer[1024];
+in = open(go, O_RDONLY);
+if (in == -1)
+_error1(go);
+out = open(to, O_CREAT | O_WRONLY | O_TRUNC, 0664);
+if (out == -1)
+{
+dprintf(STDERR_FILENO, "Error: Can't write to 1 %s", to);
+exit(99);
+}
+do {
+i = read(in, buffer, 1024);
+if (i == -1)
+_error1(go);
+j = read(out, buffer, i);
+if (j == -1 || j != i)
+{
+dprintf(STDERR_FILENO, "Error: Can't write to 2 %s", to);
+exit(99);
+}
+} while (i == 1024);
+if (close(in) == -1)
+{
+dprintf(STDERR_FILENO, "Error: Can't close fd %s", go);
+exit(100);
+}
+if  (close(out) == -1)
+{
+dprintf(STDERR_FILENO, "Error: Can't close fd %s", to);
+exit(100);
+}
+return (0);
+}
--- 0x15-file_io/holberton.h
+++ 0x15-file_io/holberton.h
@@ -11,4 +11,6 @@ int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+void _error1(char *xo);
+int cp(char *go, char *to);
 #endif
