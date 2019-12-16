92aca000fb7181ff90e7ba76c4930f10d12da7be
elisa2105 <aikomi1726@gmail.com>
2019-12-16 21:47:36 +0000

    fixed prototype

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -1,12 +1,12 @@
 #include "lists.h"
 /**
- * sum_listint - sums the data in the linked list
+ * sum_dlistint - sums the data in the linked list
  * @head: head of the structure
  * Return: if empty returns 0 otherwise the sum
  */
-int sum_listint(listint_t *head)
+int sum_dlistint(dlistint_t *head)
 {
-listint_t *list = head;
+dlistint_t *list = head;
 int i = 0;
 
 if (head == NULL)
