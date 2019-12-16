127f2d8200c587adea8c4a509047f6e3dc1c8b95
elisa2105 <aikomi1726@gmail.com>
2019-12-16 21:44:04 +0000

    fixed betty

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -12,7 +12,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 dlistint_t *nnode;
 dlistint_t *tmp = *head;
 if (head == NULL)
-return(NULL);
+return (NULL);
 nnode = malloc(sizeof(dlistint_t));
 if (nnode == NULL)
 return (NULL);
