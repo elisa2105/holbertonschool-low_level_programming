e1e999fe8cef5777effb594746486b9bdb6d168c
elisa2105 <aikomi1726@gmail.com>
2020-02-21 21:25:52 +0000

    perimeter

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Interview prep question island perimeter
+"""
+
+def  island_perimeter(grid):
+    """
+    Function to return the perimeter
+    Arguments:
+    grid -a matrix of 1's
+    """
+    l = 1
+    w = 0
+    perimeter = 0
+    for y, y_axis in enumerate(grid):
+        for x, x_axis in enumerate(y_axis):
+            if x_axis == l:
+                if x == w or grid[y][x-1] == w:
+                    perimeter += 1
+                if y == w or grid[y-1][x] == w:
+                    perimeter += 1
+                if x + 1 == len(y_axis) or grid[y][x+1] == w:
+                    perimeter += 1
+                if y + 1 == len(grid) or grid[y+1][x] == w:
+                    perimeter += 1
+    return perimeter
